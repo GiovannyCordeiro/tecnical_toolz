22237256aab5e64de280b69518ed7852c96071d4
Giovanny Cordeiro <giovannycordeiropb@gmail.com>
2025-03-19 23:40:44 +0000

    feat: model rooms

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_19_232522) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_19_233956) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "rooms", force: :cascade do |t|
+    t.string "name"
+    t.boolean "is_private", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
