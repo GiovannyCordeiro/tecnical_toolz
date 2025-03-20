c9f9ecacb2a4a0279f2e3c86336e790e13d9e04b
Giovanny Cordeiro <giovannycordeiropb@gmail.com>
2025-03-20 03:51:04 +0000

    feat: adding mesage setup

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   root "pages#home"
   get "user/:id", to: "users#show", as: "user"
 
-  resources :rooms
+  resources :rooms do
+    resources :messages
+  end
 
   devise_for :users
   devise_scope :user do
